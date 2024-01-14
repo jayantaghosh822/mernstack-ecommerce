8eb8295ce9449f455697b001ec48524902f4c4e1
argha10111998 <156571144+jayantaghosh822@users.noreply.github.com>
2024-01-14 16:01:53 +0530

    Update index.js

--- E-COMMERCE-APIs/index.js
+++ E-COMMERCE-APIs/index.js
@@ -1,13 +1,57 @@
+// var express = require('express');
+// var app = express();
+
+// app.get('/', function (req, res) {
+// res.send('Hello World');
+// })
+// app.listen(8000);
+
+// import express from 'express';
+// import connect_db from './config/db';
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.use(express.static('public'));
+app.use(express.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+  }));
+const cors = require('cors');
 
-app.get('/', function (req, res) {
-res.send('Hello World');
-})
-app.listen(8000);
+app.use(cors({
+    origin:["https://mernstack-ecommerce-seven.vercel.app/"],
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
+    credentials:true
+}));
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+  });
+const data_base = require("./config/db");
+data_base.Connect_db();
+ const authRoutes = require("./routes/authRoute");
+ const categoryRoutes = require("./routes/categoryRoutes");
+ const productRoutes = require("./routes/productRoute");
+// app.post('/api/v1/auth/register', (req, res) => { 
+//     console.log(req);
+// }); 
+app.use("/api/v1/auth/",authRoutes.router);
+app.use("/api/v1/auth/",categoryRoutes.router);
+app.use("/api/v1/auth/",productRoutes.router);
+var ejs = require('ejs');
 
-// app.listen(8000, function () {
-// var host = server.address().address;
-// var port = server.address().port;
-// console.log("Example app listening at http://%s:%s", host, port);
-// })
+
+
+
+
+var bodyParser = require('body-parser');
+app.listen(5000);
+
+
+
+
+ 
