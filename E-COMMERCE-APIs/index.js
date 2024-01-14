56cedc1aee325da97f81e0d35629ab96d6e49d04
argha10111998 <156571144+jayantaghosh822@users.noreply.github.com>
2024-01-14 22:57:06 +0530

    srees ram

--- E-COMMERCE-APIs/index.js
+++ E-COMMERCE-APIs/index.js
@@ -4,7 +4,7 @@ var app = express();
 app.get('/', function (req, res) {
 res.send('Hello World');
 });
-app.listen(8000);
+app.listen(5000);
 
 // const data_base = require("./config/db");
 // data_base.Connect_db();
