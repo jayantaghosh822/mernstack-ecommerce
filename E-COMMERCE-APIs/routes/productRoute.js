befb980ce77e8a56bacdcf5229114af4424217ed
argha10111998 <156571144+jayantaghosh822@users.noreply.github.com>
2024-01-21 00:43:18 +0530

    sia raam

--- E-COMMERCE-APIs/routes/productRoute.js
+++ E-COMMERCE-APIs/routes/productRoute.js
@@ -28,7 +28,7 @@ var express = require('express');
 const router = express.Router();
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, "/images/")
+      cb(null, "/img/")
     },
     filename: (req, file, cb) => {
       cb(null, Date.now() + "-" + file.originalname)
