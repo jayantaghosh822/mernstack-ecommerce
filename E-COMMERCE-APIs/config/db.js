944854abf33521ef38058a25b55fbbb1e6a3915e
jayantaghosh822 <156571144+jayantaghosh822@users.noreply.github.com>
2024-01-21 13:48:05 +0530

    sia raam

--- E-COMMERCE-APIs/config/db.js
+++ E-COMMERCE-APIs/config/db.js
@@ -2,8 +2,8 @@
 var express = require('express');
 const mongoose = require('mongoose');
 
-const uri = "mongodb+srv://renderaccess:sc5no9DyVh9lcs5w@cluster0.fyhrqyd.mongodb.net/e-commerce?retryWrites=true&w=majority";
-// const uri = "mongodb+srv://vercel-admin-user-65a42be41fcdc35ed0f17878:XTBsRgoMt5i27Zcn@cluster0.0o7bjqw.mongodb.net/ecommerce?retryWrites=true&w=majority";
+// const uri = "mongodb+srv://renderaccess:sc5no9DyVh9lcs5w@cluster0.fyhrqyd.mongodb.net/e-commerce?retryWrites=true&w=majority";
+ const uri = "mongodb+srv://vercel-admin-user-65a42be41fcdc35ed0f17878:XTBsRgoMt5i27Zcn@cluster0.0o7bjqw.mongodb.net/ecommerce?retryWrites=true&w=majority";
 // mongoose.connect('mongodb://127.0.0.1:27017/ecommerce');
 const Connect_db = async()=>{
     try{
