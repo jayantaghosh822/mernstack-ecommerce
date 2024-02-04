dbb2550992f698c5fa54e76e00d4f9bc1e75b5b3
jayantaghosh822 <156571144+jayantaghosh822@users.noreply.github.com>
2024-02-04 22:44:32 +0530

    Update index.js

--- E-COMMERCE-APIs/index.js
+++ E-COMMERCE-APIs/index.js
@@ -11,12 +11,12 @@ app.use(bodyParser.urlencoded({
 const cors = require('cors');
 app.use(cors({origin: process.env.CLIENT_URL})); 
 app.use(cors({
-    origin:["http://localhost:3000","https://deluxe-faloodeh-682d02.netlify.app/"],
+    origin:["http://localhost:3000","https://deluxe-faloodeh-682d02.netlify.app"],
     methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
     credentials:true
 }));
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000','https://deluxe-faloodeh-682d02.netlify.app/');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000','https://deluxe-faloodeh-682d02.netlify.app');
     res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept'
