5ce1e8fb062d44fc13f05d07acd43991f77aa961
jayantaghosh822 <156571144+jayantaghosh822@users.noreply.github.com>
2024-02-04 22:15:52 +0530

    Update index.js

--- E-COMMERCE-APIs/index.js
+++ E-COMMERCE-APIs/index.js
@@ -16,7 +16,7 @@ app.use(cors({
     credentials:true
 }));
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000','https://e-commerce-server-ry9p.onrender.com');
     res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept'
